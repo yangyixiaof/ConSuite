6a68ea7d077e7c53733477b5bd43d5d09ef3291e
DELL <DELL@DESKTOP-20DEPKI>
2017-01-11 17:05:56 +0800

    small change.

--- ConSuite/src/cn/yyx/research/integrate/ConcatMain.java
+++ ConSuite/src/cn/yyx/research/integrate/ConcatMain.java
@@ -29,11 +29,11 @@ public class ConcatMain {
 	private ArrayList<String> refined_args = new ArrayList<String>();
 
 	public ConcatMain(String[] args) {
-		for (String arg : args)
-		{
-			System.err.println("arg:" + arg);
-		}
-		System.exit(1);
+//		for (String arg : args)
+//		{
+//			System.err.println("arg:" + arg);
+//		}
+//		System.exit(1);
 		for (int i = 0; i < args.length; i++) {
 			String one_arg = args[i].trim();
 			if (one_arg.startsWith("-Djava")) {
