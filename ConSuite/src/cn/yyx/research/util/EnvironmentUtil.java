1905ee0d2e30996205efe4cb2959d4da25245628
DELL <DELL@DESKTOP-20DEPKI>
2017-01-17 16:15:47 +0800

    small change.

--- ConSuite/src/cn/yyx/research/util/EnvironmentUtil.java
+++ ConSuite/src/cn/yyx/research/util/EnvironmentUtil.java
@@ -14,7 +14,11 @@ public class EnvironmentUtil {
 		try {
 			String replaced = map.get("JAVA_HOME").replace('\\', '/');
 			map.put("JAVA_HOME", map.get("JAVA_HOME").replace('\\', '/').replace(replaced, JAVA_HOME));
-			map.put("PATH", map.get("PATH").replace('\\', '/').replace(replaced, JAVA_HOME));
+			String path = "Path";
+			if (map.get("PATH") != null && !map.get("PATH").equals("")) {
+				path = "PATH";
+			}
+			map.put(path, map.get(path).replace('\\', '/').replace(replaced, JAVA_HOME));
 			map.put("CLASSPATH", map.get("CLASSPATH").replace('\\', '/').replace(replaced, JAVA_HOME));
 		} catch (Exception e) {
 			System.err.println(map.get("Path"));
