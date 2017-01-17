781b2c9a4b2696a5604ad0bdec6f9f48ead54213
DELL <DELL@DESKTOP-20DEPKI>
2017-01-17 17:25:48 +0800

    small change.

--- ConSuite/src/cn/yyx/research/util/EnvironmentUtil.java
+++ ConSuite/src/cn/yyx/research/util/EnvironmentUtil.java
@@ -9,17 +9,31 @@ public class EnvironmentUtil {
 		return System.getProperty("os.name").toLowerCase().indexOf("windows")>=0;
 	}
 	
-	public static void HandleProcessEnvironment(Map<String, String> map, String JAVA_HOME)
+	public static void HandleProcessEnvironment(Map<String, String> map, String JAVA_HOME_REP)
 	{
 		try {
 			String replaced = map.get("JAVA_HOME").replace('\\', '/');
-			map.put("JAVA_HOME", map.get("JAVA_HOME").replace('\\', '/').replace(replaced, JAVA_HOME));
+			String replace = JAVA_HOME_REP.replace('\\', '/');
+			
+			if (map.get("JAVA_HOME") == null || map.get("JAVA_HOME").equals(""))
+			{
+				System.err.println("No java home?");
+				System.exit(1);
+			}
+			map.put("JAVA_HOME", map.get("JAVA_HOME").replace('\\', '/').replace(replaced, replace));
+			
 			String path = "Path";
 			if (map.get("PATH") != null && !map.get("PATH").equals("")) {
 				path = "PATH";
 			}
-			map.put(path, map.get(path).replace('\\', '/').replace(replaced, JAVA_HOME));
-			map.put("CLASSPATH", map.get("CLASSPATH").replace('\\', '/').replace(replaced, JAVA_HOME));
+			if (map.get(path) != null)
+			{
+				map.put(path, map.get(path).replace('\\', '/').replace(replaced, replace));
+			}
+			if (map.get("CLASSPATH") != null)
+			{
+				map.put("CLASSPATH", map.get("CLASSPATH").replace('\\', '/').replace(replaced, replace));
+			}
 		} catch (Exception e) {
 			System.err.println(map.get("Path"));
 			System.err.println(map.get("PATH"));
